235e15e1ffce4b3fed14db70439a4713e20f82d7
Pooja Jadhav <poojajadhavpune@gmail.com>
2024-07-26 21:18:33 +0530

    singleton

---
+++ practice/singleton.py
@@ -0,0 +1,12 @@
+
+#TODO correct way to implement this will be @lru_cache(maxsize=None) ref:https://stackoverflow.com/a/73988733
+class Singleton:
+    instance = None
+    def __init__(self, x) -> None:
+        self.x = x
+
+    @staticmethod
+    def get_instance():
+        if not Singleton.instance:
+            Singleton.instance = Singleton(5)
+        return Singleton.instance
