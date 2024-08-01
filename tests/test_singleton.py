20b3619f652e6c6a93524c2e655facece5621438
Pooja Jadhav <poojajadhavpune@gmail.com>
2024-08-02 01:49:59 +0530

    Update test_singleton.py

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -2,6 +2,6 @@ from practice.singleton import Singleton
 
 
 def test_singleton():
-    first = Singleton()
-    second = Singleton()
+    first = Singleton(1)
+    second = Singleton(1)
     assert first is second
